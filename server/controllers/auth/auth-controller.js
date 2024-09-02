39758056dc2891c2e1119b8ceeb75f75b90a457c
sangammukherjee <sangamukherjee@gmail.com>
2024-09-02 14:22:15 +0530

    changes cookie config

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -68,7 +68,7 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully",
       user: {
